0a86adf62be5bc8418e4d19f470b260a8930951f
aTo <aTo@ato_mac.home>
2016-04-13 12:41:09 +0200

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by aTo on 13/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by aTo on 13/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
